47a4f090e5a0121f696e3e94ac52f6b119fbdfdb
RomanKosmyna <roma.kosmyna@gmail.com>
2023-11-23 23:33:04 +0200

    Added new branch (roma-branch) with a new project

---
+++ homework7/roma-branch-git-hw3-task4/Program.cs
@@ -0,0 +1,32 @@
+﻿double result = default;
+double number1;
+double number2;
+
+bool switcher = false;
+
+Console.WriteLine("Please, choose first operand");
+bool parseFirstNumber = double.TryParse(Console.ReadLine(), out number1);
+Console.WriteLine("Please, choose second operand");
+bool parseSecondNumber = double.TryParse(Console.ReadLine(), out number2);
+Console.WriteLine("Please, choose operator");
+string @operator = Console.ReadLine();
+
+if (@operator != "+" && @operator != "-" && @operator != "*" && @operator != "/")
+{
+    Console.WriteLine("Only following operators are legit: +, -, *, /");
+    return;
+}
+
+if (parseFirstNumber && parseSecondNumber)
+{
+    switcher = true;
+}
+
+while(switcher)
+{
+    SwitchCase switchCase = new(@operator, number1, number2, result);
+
+    switchCase.PerformSwitch();
+
+    switcher = false;
+}
